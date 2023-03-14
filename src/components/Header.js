15bc470781b9f2c8c384bded260c85eca5c18581
halderjoydeep <42032921+halderjoydeep@users.noreply.github.com>
2023-03-15 02:50:15 +0530

    Design search field

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import Image from 'next/image';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import AmazonLogo from '../../public/images/amazon_logo.png';
 
 export default function Header() {
   return (
     <header>
       {/* Top Navigation Bar */}
-      <div className="flex items-center flex-grow px-4 p-2 bg-amazon_blue">
+      <div className="flex items-center gap-4 px-4 py-2 bg-amazon_blue">
         {/* Logo */}
-        <div className="flex items-center flex-grow sm:flex-grow-0 mt-2 ">
+        <div className="flex items-center flex-grow sm:flex-grow-0 mt-2">
           <Image
             src={AmazonLogo}
             width={97}
@@ -15,6 +16,16 @@ export default function Header() {
             alt="Amazon Logo"
             className="object-contain cursor-pointer"
           />
+        </div>
+
+        {/* Search Field */}
+        <div className="hidden sm:flex items-center flex-1 h-10 rounded-md overflow-hidden">
+          <input
+            type="text"
+            className="h-full flex-1 px-4 py-2 focus:outline-none "
+            placeholder="Search Amazon"
+          />
+          <MagnifyingGlassIcon className="h-full p-2 bg-amber-400 hover:bg-amber-600 cursor-pointer" />
         </div>
       </div>
 
