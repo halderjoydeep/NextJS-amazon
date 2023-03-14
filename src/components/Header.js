67e615181adccd3a1d98cd37fe481f9de51ed2d9
halderjoydeep <42032921+halderjoydeep@users.noreply.github.com>
2023-03-15 03:33:46 +0530

    Design right side navigations

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
 import Image from 'next/image';
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import {
+  MagnifyingGlassIcon,
+  ShoppingCartIcon,
+} from '@heroicons/react/24/outline';
 import AmazonLogo from '../../public/images/amazon_logo.png';
 
 export default function Header() {
   return (
     <header>
       {/* Top Navigation Bar */}
-      <div className="flex items-center gap-4 px-4 py-2 bg-amazon_blue">
+      <div className="flex items-center flex-1 px-4 py-2 bg-amazon_blue">
         {/* Logo */}
-        <div className="flex items-center flex-grow sm:flex-grow-0 mt-2">
+        <div className="flex items-center flex-grow sm:flex-grow-0 mt-2 mr-6">
           <Image
             src={AmazonLogo}
             width={97}
@@ -19,7 +22,7 @@ export default function Header() {
         </div>
 
         {/* Search Field */}
-        <div className="hidden sm:flex items-center flex-1 h-10 rounded-md overflow-hidden">
+        <div className="hidden sm:flex items-center flex-1 h-10 rounded-md overflow-hidden mr-6">
           <input
             type="text"
             className="h-full flex-1 px-4 py-2 focus:outline-none "
@@ -27,10 +30,32 @@ export default function Header() {
           />
           <MagnifyingGlassIcon className="h-full p-2 bg-amber-400 hover:bg-amber-600 cursor-pointer" />
         </div>
-      </div>
 
-      {/* Bottom Navigation Bar */}
-      <div></div>
+        {/* Right Side */}
+        <div className="flex items-center justify-between space-x-8 text-white whitespace-nowrap">
+          {/* Accounts & Lists */}
+          <div className="link">
+            <p className="text-gray-100 text-xs font-medium">Hello, Joydeep!</p>
+            <p className="font-bold text-sm">Account & Lists</p>
+          </div>
+          {/* Returns & Orders */}
+          <div className="link">
+            <p className="text-gray-100 text-xs font-medium">Returns</p>
+            <p className="font-bold text-sm">& Orders</p>
+          </div>
+          {/* Basket */}
+          <div className="relative flex items-center link">
+            <ShoppingCartIcon className="h-8" />
+            <p className="hidden md:block font-bold text-sm mt-2">Cart</p>
+            <span className="absolute -top-1 left-5 flex items-center justify-center w-4 h-4 p-1 text-xs text-black font-bold bg-amber-500 rounded-full">
+              0
+            </span>
+          </div>
+        </div>
+
+        {/* Bottom Navigation Bar */}
+        <div></div>
+      </div>
     </header>
   );
 }
